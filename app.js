9a2addbd3ec8e13b8bf2f06079990894b355e0c7
faizanali880 <130624777+faizanali880@users.noreply.github.com>
2023-05-14 16:57:55 +0500

    rabbitMQ implementations

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ const express = require('express')
 const app = express()
 const mongoose = require("mongoose");
 const connectDB = require('./dbconnection');
-
+const amqp = require("amqplib");
+var channel, connection;
 connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port {PORT}`));
+
+connectQueue()  // call the connect function
+ 
+async function connectQueue() {
+    try {
+        connection = await amqp.connect("amqp://127.0.0.1");
+        channel    = await connection.createChannel()
+        
+        await channel.assertQueue("notifications")
+        
+        channel.consume("notifications", data => {
+            console.log(`${Buffer.from(data.content)}`);
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/testDB`, {
+      // const url =
+      // "mongodb+srv://root:" +
+      // process.env.MONGO_ATLAS_PASS +
+      // "@docdb-2022-02-17-21-16-45.cluster-c6uvus96jlgd.eu-central-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+    
+
+      const conn = await mongoose.connect(`mongodb://localhost:27017/local_nuavto`, {
         useNewUrlParser: true,
       });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
+      console.log(`MongoDB Connected:` ,conn.connection.host);
     } catch (error) {
       console.error(error.message);
       process.exit(1);
