1bc6668cec2eb837a8269f85484c633a6a02efec
faizanali880 <130624777+faizanali880@users.noreply.github.com>
2023-07-26 13:13:31 +0500

    fixes

--- dbconnection.js
+++ dbconnection.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
       // "mongodb+srv://root:" +
       // process.env.MONGO_ATLAS_PASS +
       // "@docdb-2022-02-17-21-16-45.cluster-c6uvus96jlgd.eu-central-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
-    
 
-      const conn = await mongoose.connect(`mongodb://localhost:27017/local_nuavto`, {
+
+      const conn = await mongoose.connect(`mongodb://localhost:27017/admin`, {
         useNewUrlParser: true,
       });
       console.log(`MongoDB Connected:` ,conn.connection.host);
